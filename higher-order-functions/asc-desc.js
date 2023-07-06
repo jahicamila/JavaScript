9248c1a4205765163fa6bcb73ccfaefb599297b0
Amila Jahić <102929605+jahicamila@users.noreply.github.com>
2023-07-06 11:49:02 +0200

    Update asc-desc.js

--- higher-order-functions/asc-desc.js
+++ higher-order-functions/asc-desc.js
@@ -2,36 +2,36 @@
 //osnovu uzlaznog (asc) ili silaznog (desc) redoslijeda.
 
 function reorderDigits(arr, order) {
-    let noviNiz = [];
-    if(order == "asc"){
-        for(let i=0; i<arr.length; ++i){
-            let broj = asc(arr[i]);
-            noviNiz.push(broj);
-        }
-        return noviNiz;
-    } else if (order == "desc") {
-        for(let i=0; i<arr.length; ++i){
-            let broj = desc(arr[i]);
-            noviNiz.push(broj);
-        }
-        return noviNiz;
-    }
+     let newString = [];
+     if(order == "asc"){
+         for(let i=0; i<arr.length; ++i){
+             let number = asc(arr[i]);
+             newString.push(number);
+         }
+         return newString;
+     } else if (order == "desc") {
+         for(let i=0; i<arr.length; ++i){
+             let number = desc(arr[i]);
+             newString.push(number);
+         }
+         return newString;
+     }
 }
 
-function asc(broj){
-    const cifre = broj.toString().split('');
-    cifre.sort((a,b) => a-b);
-    let x = [parseInt(cifre.join(''))];
-    return x;
+function asc(number){
+     const digits = number.toString().split('');
+     digits.sort((a,b) => a-b);
+     let x = [parseInt(digits.join(''))];
+     return x;
 }
 
-function desc(broj){
-    const cifre = broj.toString().split('');
-    cifre.sort((a,b) => b-a);
-    let x = [parseInt(cifre.join(''))];
-    return x;
+function desc(number){
+     const digits = number.toString().split('');
+     digits.sort((a,b) => b-a);
+     let x = [parseInt(digits.join(''))];
+     return x;
 }
 
-let niz = [515, 341, 98, 44, 112];
-let sortiranNiz = reorderDigits(niz, "desc")
-console.log(sortiranNiz);
+let string = [515, 341, 98, 44, 112];
+let sortedString = reorderDigits(string, "desc")
+console.log(sortedString);
